13bb58e18e296e1ef6704bafb77fc670b81b4207
Arthur Chystyak <artur.chistyak@outlook.com>
2016-09-08 14:52:30 +0300

    Initial Commit

---
+++ PlanAndDo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PlanAndDo
+//
+//  Created by Arthur Chistyak on 08.09.16.
+//  Copyright © 2016 TodoTeamGroup. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
