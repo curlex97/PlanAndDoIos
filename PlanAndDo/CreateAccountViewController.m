3c00c71d583105455c25976696a7c8b5f5ba558c
Arthur Chystyak <artur.chistyak@outlook.com>
2016-09-13 15:03:38 +0300

    add create account (1.0)

---
+++ PlanAndDo/CreateAccountViewController.m
@@ -0,0 +1,55 @@
+//
+//  CreateAccountViewController.m
+//  PlanAndDo
+//
+//  Created by Arthur Chistyak on 13.09.16.
+//  Copyright © 2016 TodoTeamGroup. All rights reserved.
+//
+
+#import "CreateAccountViewController.h"
+#import "KSApplicatipnColor.h"
+
+#define TEXTFIELD_PADDING_LEFT 10
+
+
+@interface CreateAccountViewController ()
+
+@end
+
+@implementation CreateAccountViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    CAGradientLayer * gradient=[KSApplicatipnColor sharedColor].rootGradient;
+    gradient.frame=self.view.bounds;
+    [self.tableView removeFromSuperview];
+    self.view.backgroundColor=[UIColor whiteColor];
+    [self.view.layer insertSublayer:gradient atIndex:0];
+    
+    UIView *emailPaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, TEXTFIELD_PADDING_LEFT, 0)];
+    self.emailTextField.leftView = emailPaddingView;
+    self.emailTextField.leftViewMode = UITextFieldViewModeAlways;
+    
+    UIView *usernamePaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, TEXTFIELD_PADDING_LEFT, 0)];
+    self.usernameTextField.leftView = usernamePaddingView;
+    self.usernameTextField.leftViewMode = UITextFieldViewModeAlways;
+    
+    UIView *passwordPaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, TEXTFIELD_PADDING_LEFT, 0)];
+    self.passwordTextField.leftView = passwordPaddingView;
+    self.passwordTextField.leftViewMode = UITextFieldViewModeAlways;
+    
+    UIView *reenterPasswordPaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, TEXTFIELD_PADDING_LEFT, 0)];
+    self.reenterPasswordTextField.leftView = reenterPasswordPaddingView;
+    self.emailTextField.leftViewMode = UITextFieldViewModeAlways;
+    
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
