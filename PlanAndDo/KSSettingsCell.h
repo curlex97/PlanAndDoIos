de2893f1b92a25a16308f2c11a10af16b1952713
Arthur Chystyak <artur.chistyak@outlook.com>
2016-09-15 12:23:00 +0300

    Settings Page

---
+++ PlanAndDo/KSSettingsCell.h
@@ -0,0 +1,16 @@
+//
+//  KSSettingsCell.h
+//  PlanAndDo
+//
+//  Created by Arthur Chistyak on 15.09.16.
+//  Copyright © 2016 TodoTeamGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSSettingsCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *paramNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *paramValueLabel;
+
+@end
--- PlanAndDo/SettingsViewController.m
+++ PlanAndDo/SettingsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SettingsViewController.h"
+#import "KSSettingsCell.h"
 
 @interface SettingsViewController()<UITableViewDelegate, UITableViewDataSource>
 @end
@@ -29,26 +30,35 @@
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell* cell = [[UITableViewCell alloc] init];
+    NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"KSSettingsCell"owner:self options:nil];
+    KSSettingsCell * cell=[nib objectAtIndex:0];
+    
+    cell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;
+
     
     switch (indexPath.row) {
         case 0:
-            cell.textLabel.text = @"Start page";
+            cell.paramNameLabel.text = @"Start page";
             break;
         case 1:
-            cell.textLabel.text = @"Format date";
+            cell.paramNameLabel.text = @"Format date";
             break;
         case 2:
-            cell.textLabel.text = @"Format time";
+            cell.paramNameLabel.text = @"Format time";
             break;
         case 3:
-            cell.textLabel.text = @"Start day";
+            cell.paramNameLabel.text = @"Start day";
             break;
         default:
             break;
     }
     
     return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50;
 }
 
 
