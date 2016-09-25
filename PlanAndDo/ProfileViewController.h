6c82c97257770284b338ddd2b86e8fca99e4ba18
Vlad Nagirnyak <v.nagirnyak@keepsolid.com>
2016-09-25 12:10:34 +0300

    Add profile page!

---
+++ PlanAndDo/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  PlanAndDo
+//
+//  Created by Student on 9/23/16.
+//  Copyright © 2016 TodoTeamGroup. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface ProfileViewController : BaseTableViewController
+
+@end
---
+++ PlanAndDo/ProfileViewController.m
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.m
+//  PlanAndDo
+//
+//  Created by Student on 9/23/16.
+//  Copyright © 2016 TodoTeamGroup. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
