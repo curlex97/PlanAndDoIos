6b3c39f3e5b0ec90bbbb44c00d064e1571913273
Arthur Chystyak <artur.chistyak@outlook.com>
2016-09-30 16:35:53 +0300

    Tasks and subtasks api test -FAIL

--- PlanAndDo/SubTasksApiManager.m
+++ PlanAndDo/SubTasksApiManager.m
@@ -7,6 +7,9 @@
 //
 
 #import "SubTasksApiManager.h"
+#import "UserApplicationManager.h"
+#import "FileManager.h"
+
 
 @implementation SubTasksApiManager
 
@@ -28,9 +31,27 @@
 
 -(void)syncSubTasksWithCompletion:(void (^)(NSDictionary*))completed
 {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        completed(nil);
-    });
+    KSAuthorisedUser* user = [[[UserApplicationManager alloc] init] authorisedUser];
+    
+    NSMutableDictionary* puser = [NSMutableDictionary dictionary];
+    NSMutableDictionary* inData = [NSMutableDictionary dictionary];
+    
+    NSNumber *number = [NSNumber numberWithInteger:[[FileManager readLastSyncTimeFromFile] intValue]];
+    
+    [inData setValue:number forKey:@"lst"];
+    
+    [puser setValue:[NSNumber numberWithInteger:user.ID] forKey:@"user_id"];
+    [puser setValue:[[[UIDevice currentDevice] identifierForVendor] UUIDString] forKey:@"device_id"];
+    [puser setValue:[FileManager readTokenFromFile] forKey:@"token"];
+    [puser setValue:@"sync" forKey:@"class"];
+    [puser setValue:@"syncSubtasks" forKey:@"method"];
+    
+    [puser setValue:inData forKey:@"data"];
+    
+    [self dataByData:puser completion:^(NSData * data) {
+        NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+        completed(json);
+    }];
 }
 
 @end
