cf35ec11000b767ce31bcbc312d71d84f968d3d1
Arthur Chystyak <artur.chistyak@outlook.com>
2016-10-03 12:37:11 +0300

    sync from server User and Settings

--- PlanAndDo/SyncApplicationManager.m
+++ PlanAndDo/SyncApplicationManager.m
@@ -9,6 +9,8 @@
 #import "SyncApplicationManager.h"
 #import "ApplicationDefines.h"
 #import "UserApplicationManager.h"
+#import "FileManager.h"
+#import "SettingsApplicationManager.h"
 
 @implementation SyncApplicationManager
 
@@ -32,17 +34,55 @@
 
 -(void)syncUserWithCompletion:(void (^)(bool))completed
 {
-    [[[UserApiManager alloc] init] syncUserWithCompletion:^(NSDictionary* status) {
-        [[NSNotificationCenter defaultCenter] postNotificationName:NC_SYNC_USER object:nil];
-        completed(YES);
+    [[[UserApiManager alloc] init] syncUserWithCompletion:^(NSDictionary* dictionary) {
+        
+        NSString* status = [dictionary valueForKeyPath:@"status"];
+        
+        if([status containsString:@"suc"])
+        {
+            NSUInteger ID = [[dictionary valueForKeyPath:@"data.id"] integerValue];
+            NSString* userName = [dictionary valueForKeyPath:@"data.name"];
+            int syncStatus = [[dictionary valueForKeyPath:@"data.user_sync_status"] intValue];
+            NSString* email = [dictionary valueForKeyPath:@"data.email"];
+            NSDate *createDate = [NSDate dateWithTimeIntervalSince1970:[[dictionary valueForKeyPath:@"data.created_at"] intValue]];
+            NSDate *lastVisitDate = [NSDate dateWithTimeIntervalSince1970:[[dictionary valueForKeyPath:@"data.lastvisit_date"] intValue]];
+
+            KSAuthorisedUser* user = [[KSAuthorisedUser alloc] initWithUserID:ID andUserName:userName andEmailAdress:email andCreatedDeate:createDate andLastVisitDate:lastVisitDate andSyncStatus:syncStatus andAccessToken:[FileManager readTokenFromFile] andUserSettings:nil];
+            
+            [[[UserCoreDataManager alloc] init] syncSetUser:user];
+            [[NSNotificationCenter defaultCenter] postNotificationName:NC_SYNC_USER object:nil];
+
+            completed(true);
+        }
+        completed(false);
+
+    
     }];
 }
 
 -(void)syncSettingsWithCompletion:(void (^)(bool))completed
 {
-    [[[SettingsApiManager alloc] init] syncSettingsWithCompletion:^(NSDictionary* status) {
-        [[NSNotificationCenter defaultCenter] postNotificationName:NC_SYNC_SETTINGS object:nil];
-        completed(YES);
+    [[[SettingsApiManager alloc] init] syncSettingsWithCompletion:^(NSDictionary* dictionary) {
+        
+        NSString* status = [dictionary valueForKeyPath:@"status"];
+        
+        if([status containsString:@"suc"])
+        {
+        
+            NSUInteger settingsID = [[dictionary valueForKeyPath:@"data.id"] integerValue];
+            NSString* startPage = [dictionary valueForKeyPath:@"data.start_page"];
+            NSString* dateFormat = [dictionary valueForKeyPath:@"data.date_format"];
+            NSString* timeFormat = [dictionary valueForKeyPath:@"data.time_format"];
+            NSString* pageType = [dictionary valueForKeyPath:@"data.page_type"];
+            NSString* startDay = [dictionary valueForKeyPath:@"data.start_day"];
+            int syncStatus = [[dictionary valueForKeyPath:@"data.settings_sync_status"] intValue];
+            
+            UserSettings *settings = [[UserSettings alloc] initWithID:settingsID andStartPage:startPage andDateFormat:dateFormat andPageType:pageType andTimeFormat:timeFormat andStartDay:startDay andSyncStatus:syncStatus];
+            [[[SettingsCoreDataManager alloc] init] syncSetSettings:settings];
+            [[NSNotificationCenter defaultCenter] postNotificationName:NC_SYNC_SETTINGS object:nil];
+        }
+        
+        
     }];
 }
 
